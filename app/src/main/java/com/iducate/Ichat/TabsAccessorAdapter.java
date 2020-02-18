94e888020be7ccd812cd8937dd64c96927e5d0da
SHAANRAASTI <shanrasti1@gmail.com>
2020-02-18 17:16:48 +0530

    ichat push1

---
+++ app/src/main/java/com/iducate/Ichat/TabsAccessorAdapter.java
@@ -0,0 +1,65 @@
+package com.iducate.Ichat;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TabsAccessorAdapter extends FragmentPagerAdapter {
+     public TabsAccessorAdapter(@NonNull FragmentManager fm) {
+         super(fm,BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+
+     }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int i)
+    {
+        switch (i)
+        {
+            case 0:
+                ChatsFragment chatsFragment = new ChatsFragment();
+                return chatsFragment;
+
+            case 1:
+                GroupsChatFragement groupsChatFragement = new GroupsChatFragement();
+                return groupsChatFragement;
+
+            case 2:
+                Contactsfragment contactsfragment = new Contactsfragment();
+
+                return contactsfragment;
+
+                default:
+                    return null;
+        }
+    }
+
+
+    @Override
+    public int getCount()
+    {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position)
+    {
+
+        switch (position)
+        {
+            case 0:
+               return "Chats";
+
+            case 1:
+              return "Groups";
+
+            case 2:
+                return  "Contacts";
+
+            default:
+                return null;
+    }
+}}
